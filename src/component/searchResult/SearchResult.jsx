2f125cfb68da320052e0bc281573dd7121e40208
Nabin khanal <khanalnabin310@gmail.com>
2024-02-23 21:06:19 +0545

    some change in map

--- src/component/searchResult/SearchResult.jsx
+++ src/component/searchResult/SearchResult.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 
-const SearchResult = ({ FoodData}) => {
-  console.log(FoodData);
+const SearchResult = ({ FoodData }) => {
+
   return (
     <div className="food_container">
-    {
-      <div className='food_items'  key={FoodData.name}>
       {
-       FoodData.map((value)=>{
-        <div>
-        <p>{FoodData.text}</p>
+        <div className='food_items' key={FoodData.name}>
+          {
+            FoodData.map((value) => {
+              return
+              {
+                FoodData.text
+              }
+
+            })
+          }
+
         </div>
-       })
       }
-      </div>
-    }
-    
+
 
 
     </div>
